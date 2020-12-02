99644f20d257155ef1eaa6ce5384ca9fc80c4877
Mohammed Al-Mahdawi <mohammed@al-mahdawi.com>
2020-12-02 10:33:08 +0300

    WIP

--- src/components/UI/ReactTableWrapper/ReactTableWrapper.js
+++ src/components/UI/ReactTableWrapper/ReactTableWrapper.js
@@ -1,5 +1,96 @@
 import React from 'react'
+import { useTable, useFilters } from 'react-table'
 
-export default function ReactTableWrapper({}) {
-  return <div></div>
+export default function ReactTableWrapper({ columns, data }) {
+  // Define a default UI for filtering
+  function DefaultColumnFilter({
+    column: { filterValue, preFilteredRows, setFilter },
+  }) {
+    const count = preFilteredRows.length
+
+    return (
+      <input
+        className="shadow-sm px-2 py-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
+        value={filterValue || ''}
+        onChange={(e) => {
+          setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+        }}
+        placeholder={`Search ${count} roles...`}
+      />
+    )
+  }
+
+  const defaultColumn = React.useMemo(
+    () => ({
+      // Let's set up our default Filter UI
+      Filter: DefaultColumnFilter,
+    }),
+    []
+  )
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data, defaultColumn }, useFilters)
+
+  return (
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table
+              {...getTableProps()}
+              className="min-w-full divide-y divide-gray-200"
+            >
+              <thead>
+                {headerGroups.map((headerGroup) => (
+                  <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map((column) => (
+                      <th
+                        {...column.getHeaderProps()}
+                        scope="col"
+                        className="px-6 py-3 bg-gray-50 text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        {column.render('Header')}
+                        <div className="mt-1">
+                          {column.canFilter ? column.render('Filter') : null}
+                        </div>
+                      </th>
+                    ))}
+                  </tr>
+                ))}
+              </thead>
+              <tbody {...getTableBodyProps()}>
+                {rows.map((row, index) => {
+                  prepareRow(row)
+                  return (
+                    <tr
+                      {...row.getRowProps()}
+                      className={`${
+                        index % 2 !== 0 ? 'bg-gray-50' : 'bg-white'
+                      }`}
+                    >
+                      {row.cells.map((cell) => {
+                        return (
+                          <td
+                            {...cell.getCellProps()}
+                            className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
+                          >
+                            {cell.render('Cell')}
+                          </td>
+                        )
+                      })}
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/containers/Roles/RoleList/RoleList.js
+++ src/containers/Roles/RoleList/RoleList.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { FaEdit, FaTrash } from 'react-icons/fa'
 import { useTranslation } from 'react-i18next'
-import { useTable, useFilters } from 'react-table'
+import ReactTableWrapper from '../../../components/UI/ReactTableWrapper/ReactTableWrapper'
 import * as actions from '../../../store/actions/index'
 import { useUndo } from '../../../shared/react-hooks'
 import Undo from '../../../components/UI/Undo/Undo'
@@ -20,32 +20,6 @@ export function RoleList({ getAllRoles, deleteRole, roles }) {
   }, [])
 
   const allUndoListsIds = allUndoLists.map((i) => i.id)
-
-  // Define a default UI for filtering
-  function DefaultColumnFilter({
-    column: { filterValue, preFilteredRows, setFilter },
-  }) {
-    const count = preFilteredRows.length
-
-    return (
-      <input
-        className="shadow-sm px-2 py-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
-        value={filterValue || ''}
-        onChange={(e) => {
-          setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
-        }}
-        placeholder={`Search ${count} roles...`}
-      />
-    )
-  }
-
-  const defaultColumn = React.useMemo(
-    () => ({
-      // Let's set up our default Filter UI
-      Filter: DefaultColumnFilter,
-    }),
-    []
-  )
 
   const data = React.useMemo(
     () => roles.filter((u) => !allUndoListsIds.includes(u.id)),
@@ -106,80 +80,10 @@ export function RoleList({ getAllRoles, deleteRole, roles }) {
     []
   )
 
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({ columns, data, defaultColumn }, useFilters)
-
   return (
     <>
-      {/* <ReactTableWrapper
-        filterable
-        data={data}
-        columns={columns}
-        showPagination={false}
-        pageSize={data.length}
-        className="bg-white"
-      /> */}
+      <ReactTableWrapper data={data} columns={columns} />
 
-      <div className="flex flex-col">
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table
-                {...getTableProps()}
-                className="min-w-full divide-y divide-gray-200"
-              >
-                <thead>
-                  {headerGroups.map((headerGroup) => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                      {headerGroup.headers.map((column) => (
-                        <th
-                          {...column.getHeaderProps()}
-                          scope="col"
-                          className="px-6 py-3 bg-gray-50 text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >
-                          {column.render('Header')}
-                          <div className="mt-1">
-                            {column.canFilter ? column.render('Filter') : null}
-                          </div>
-                        </th>
-                      ))}
-                    </tr>
-                  ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                  {rows.map((row, index) => {
-                    prepareRow(row)
-                    return (
-                      <tr
-                        {...row.getRowProps()}
-                        className={`${
-                          index % 2 !== 0 ? 'bg-gray-50' : 'bg-white'
-                        }`}
-                      >
-                        {row.cells.map((cell) => {
-                          return (
-                            <td
-                              {...cell.getCellProps()}
-                              className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
-                            >
-                              {cell.render('Cell')}
-                            </td>
-                          )
-                        })}
-                      </tr>
-                    )
-                  })}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
       <Undo
         onUndo={onUndo}
         onDismiss={onDismiss}
